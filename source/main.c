bec3c20e2931c448d2511b037ee113700563729f
Kacper Piotr Wojakowski <kwojakow@uxn14.ia.pw.edu.pl>
2019-05-31 15:21:19 +0200

    comments

--- source/main.c
+++ source/main.c
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// Time for actions
 #define RUNTIME 30
 #define THINK 2
 #define EAT 1
 
+// No. of philosophers
 #define N 5
+// For readibility
 #define LEFT (i+N-1)%N
 #define RIGHT (i+1)%N
 
@@ -15,12 +18,15 @@
 #define HUNGRY 1
 #define EATING 2
 
+// Synchronization
 void grab_forks(int i);
 void put_away_forks(int i);
 void test(int i);
 
+// Philosopher Thread
 void* philosopher(void* num);
 
+// Mutexes and shared variables
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t s[N] = PTHREAD_MUTEX_INITIALIZER;
 int state[N];
@@ -47,21 +53,25 @@ int main(int argc, char** argv)
 		}	
 	}
 
+	// Let the simulation run
 	sleep(RUNTIME);
 	printf("Finishing threads and showing results\n");
 	for(int i=0; i<N; i++)
 	{
+		// Cancel all philosophers and wait for them to exit
 		pthread_cancel(philo[i]);
 		pthread_join(philo[i], NULL);
 		printf("Philosopher %d has left the table.\n", i);
 	}
+	// Print results
 	pthread_mutex_lock(&m);
-	printf("Meals eaten:\n");
-	for(int i=0; i<N; i++)
-	{
-		printf("Philosopher %d: %d\n", i, meals[i]);
-	}
+		printf("Meals eaten:\n");
+		for(int i=0; i<N; i++)
+		{
+			printf("Philosopher %d: %d\n", i, meals[i]);
+		}
 	pthread_mutex_unlock(&m);
+
 	free(num);
 
 	return 0;
@@ -73,11 +83,15 @@ void* philosopher(void* num)
 	printf("Philosopher %d has joined the table.\n", *i);
 	while(1)
 	{
+		// Think
 		printf("Philosopher %d is thinking...\n", *i);
 		sleep(THINK);
+		// Hungry
 		grab_forks(*i);
+		// Eating
 		printf("Philosopher %d is eating...\n", *i);
 		sleep(EAT);
+		// Think again
 		put_away_forks(*i);
 	}
 	return NULL;
@@ -85,21 +99,25 @@ void* philosopher(void* num)
 
 void grab_forks(int i)
 {
+	// Become hungry and check if can eat
 	pthread_mutex_lock(&m);
 		state[i] = HUNGRY;
 		printf("Philosopher %d is hungry and trying to take forks.\n", i);
 		test(i);
 	pthread_mutex_unlock(&m);
 
+	// Take forks
 	pthread_mutex_lock(&s[i]);
 }
 
 void put_away_forks(int i)
 {
 	pthread_mutex_lock(&m);
+		// Increase meal count and start thinking
 		++meals[i];
 		state[i] = THINKING;
 		printf("Philosopher %d has finished eating and put away his forks.\n", i);
+		// Let neighbours eat
 		test(LEFT);
 		test(RIGHT);
 	pthread_mutex_unlock(&m);
